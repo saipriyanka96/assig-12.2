88facdc31e6f283f6eaa79cc2919125062defe33
Priyanaru96 <31105187+saipriyanka96@users.noreply.github.com>
2017-10-13 10:26:01 +0530

    Add files via upload

---
+++ ValueNotFoundException.java
@@ -0,0 +1,13 @@
+package exception;
+//Package is a grouping of related types providing access protection and name
+public class ValueNotFoundException extends Exception {
+	//public keyword is used in the declaration of a class,method or field;public classes,method and fields can be accessed by the members of any class.
+	//class defines instance and class fields,methods and inner classes as well as specifying the interfaces the class implements and the immediate superclass of the class
+	//FixedStack is a class name
+	//when the value is not found then we will use this exception	
+		public ValueNotFoundException(){
+//The super keyword in java is a reference variable that is used to refer immediate parent class object
+			super();
+
+		}
+}
